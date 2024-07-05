f50d13e2ce6f80278653c7f7d8b6590f11d2e599
Silas <103305606+SilasMilch@users.noreply.github.com>
2024-07-05 14:42:04 +0200

    Create auto.cpp

---
+++ auto.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	auto x = 20;
+	auto y = 3.14;
+
+	std::cout<<x<<std::endl;
+	std::cout<<y<<std::endl;
+}
+
+
+/**
+ * using auto to automatically identify the type of a variable
+ */
