823f8bd7fbaa6b06d614ec898e37eb2543b75716
Silas <103305606+SilasMilch@users.noreply.github.com>
2024-07-05 18:02:06 +0200

    Update polymorphismus.cpp

--- späteBindung&Polymorphismus/polymorphismus.cpp
+++ späteBindung&Polymorphismus/polymorphismus.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
 #include <vector>
 
-class Shape
+class Shape	// Basisklasse Shape
 {
 public:
-	virtual void draw() const
+	virtual void draw() const	// Virtuelle Methode, die von den abgeleiteten Klassen überschrieben wird
 	{
 		std::cout << "Drawing a generic shape" << std::endl;
 	}
 
-	virtual	~Shape() {}
+	
+	virtual	~Shape() {}	 // Virtueller Destruktor, um sicherzustellen, dass der Destruktor der abgeleiteten Klassen aufgerufen wird
 };
 
 
-class Circle : public Shape
+class Circle : public Shape	// Abgeleitete Klasse Circle
 {
 public:
-
+	// Überschreiben der draw-Methode für Circle
 	void draw() const override
 	{
 		std::cout << "Drawing a circle" << std::endl;
 	}
 };
 
-class Rectangle : public Shape
+class Rectangle : public Shape	// Abgeleitete Klasse Rectangle
 {
 public:
+	// Überschreiben der draw-Methode für Rectangle
 	void draw() const override
 	{
 		std::cout << "Drawing a rectangle" << std::endl;
 	}
 };
 
+// Abgeleitete Klasse Triangle
 class Triangle : public Shape
 {
 public:
-	void draw() const override
+	
+	void draw() const override	// Überschreiben der draw-Methode für Triangle
 	{
 		std::cout << "Drawing a triangle" << std::endl;
 	}
@@ -43,18 +47,18 @@ public:
 
 int main()
 {
-	std::vector<Shape*> shapes;
+	std::vector<Shape*> shapes;	// Vektor von Zeigern auf die Basisklasse Shape
 
-	shapes.push_back(new Circle());
+	shapes.push_back(new Circle());	// Hinzufügen verschiedener Formen zum Vektor
 	shapes.push_back(new Rectangle());
 	shapes.push_back(new Triangle());
 
-	for (const auto& shape : shapes)
+	for (const auto& shape : shapes)	// Iterieren über den Vektor und die draw-Methode aufrufen
 	{
-		shape->draw();
+		shape->draw();	// Polymorpher Aufruf
 	}
 
-	for(auto& shape : shapes)
+	for(auto& shape : shapes)	// Speicher freigeben
 	{
 		delete shape;
 	}
